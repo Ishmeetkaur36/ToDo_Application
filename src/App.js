c3bcc4978803ca90033a4c4381ba75fa95101851
Ishmeet Kaur <ishmeetkaur364@gmail.com>
2025-03-17 18:12:06 +0000

    components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
 
-const app = () => {
-  return
-}
+import Header from './components/Header';
+import TodoItem from './components/TodoItem';
+import Button from './components/Button';
+
+import "./style.css";
+
+const App = () => {
+  return (
+       <div className= "todo-container">
+        <Header title="todie app"/>
+        <TodoItem text="eat" />
+        <TodoItem completed = {true} text="code" />
+        <TodoItem text="play" />
+        <TodoItem text="sleep" />
+        <TodoItem text="study" />
+        <Button />
+       </div>
+  );
+};
 
 export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Button = () => {
+    return <button className= 'todo-button'>Click</button>
+};
+
+export default Button;
